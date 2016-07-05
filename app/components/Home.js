36dd7f7593d5018024860dad470d0efbb1d30097
Andre Almeida <almeida.andref@gmail.com>
2016-07-05 17:40:40 +0200

    initial commit

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <h2 className="text-center">
+            Search by Github Username Above
+        </h2>
+    )
+}
+
+export default Home;
---
+++ app/components/SearchGithub.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Router from 'react-router'
+
+class SearchGithub extends React.Component {
+    getRef(ref) {
+        this.usernameRef = ref;
+    }
+    handleSubmit(){
+        var username = this.usernameRef.value;
+        this.usernameRef.value = '';
+        this.props.history.pushState(null, "/profile/" + username);
+    }
+    render(){
+        return (
+            <div className="col-sm-12">
+                <form onSubmit={() => this.handleSubmit()}>
+                    <div className="form-group col-sm-7">
+                        <input type="text" className="form-control" placeholder="Search User" ref={(ref) => this.getRef()} />
+                    </div>
+                    <div className="form-group col-sm-5">
+                        <button className="btn btn-block btn-primary" type="submit">Search Github</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+SearchGithub.PropTypes ={
+    history: React.PropTypes.object.isRequired
+}
+
+export default SearchGithub;
